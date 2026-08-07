d5bc7cb43a1164c6e4137de0ffe7f957e910b671
matheus <1matheuspimentel@gmail.com>
2026-08-07 10:34:46 -0300

    Adicionar arquivos de projeto.

---
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "2t",
+                DiscountAmount = 10,
+                MinAmount = 20
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "wlrd",
+                DiscountAmount = 20,
+                MinAmount = 40
+            });
+        }
+    }
+}
---
+++ Mango.Web/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Web.Utility
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE  
+        }
+    }
+}
